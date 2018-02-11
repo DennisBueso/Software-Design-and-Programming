774e3869edf7b00145d56561f2203247794b8d3a
Dennis Bueso-Calix <dlbcalix@gmail.com>
2018-02-11 20:01:04 +0000

    Finished Accumulation.

--- src/main/scala/worksheet01/Accumulation.scala
+++ src/main/scala/worksheet01/Accumulation.scala
@@ -1,8 +1,9 @@
 package worksheet01
 
-class Accumulate {
+class Accumulation {
   def accumulate[T, R](collection: List[T], function: T => R): List[R] = {
     collection match {
+      case head :: tail => function(head) :: accumulate(tail, function)
       case _ => List.empty
     }
   }
