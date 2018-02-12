bc0e720bc0375aedf424059e6cc03965157144ac
Dennis Bueso-Calix <dlbcalix@gmail.com>
2018-02-12 13:52:42 +0000

    More testing on AnagramMatcher.

--- src/test/scala/worksheet01/AnagramMatcherTest.scala
+++ src/test/scala/worksheet01/AnagramMatcherTest.scala
@@ -8,4 +8,9 @@ class AnagramMatcherTest extends FlatSpec with Matchers {
     val anagramMatch = AnagramMatcher.matchWord(Set("hello", "world", "zombies", "pants"), "diaper")
     anagramMatch should be(Set.empty)
   }
+
+  it should "detect a simple anagram" in {
+    val anagramMatch = AnagramMatcher.matchWord(Set("tan", "stand", "at"), "ant")
+    anagramMatch should be(Set("tan"))
+  }
 }
