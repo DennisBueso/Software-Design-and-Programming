e154654eb95ffcf060c6be4077402b7cdb5b6ee3
Dennis Bueso-Calix <dlbcalix@gmail.com>
2018-02-12 13:01:25 +0000

    Finished SumOfMultiples.

--- src/test/scala/worksheet01/SumOfMultiplesTest.scala
+++ src/test/scala/worksheet01/SumOfMultiplesTest.scala
@@ -23,4 +23,49 @@ class SumOfMultiplesTest extends FlatSpec with Matchers {
     val multiplesSum = SumOfMultiples.sum(Set(3, 5), 10)
     multiplesSum should be(23)
   }
+
+  it should "sum multiples of 3 or 5 up to 100" in {
+    val multiplesSum = SumOfMultiples.sum(Set(3, 5), 100)
+    multiplesSum should be(2318)
+  }
+
+  it should "sum multiples of 3 or 5 up to 1000" in {
+    val multiplesSum = SumOfMultiples.sum(Set(3, 5), 1000)
+    multiplesSum should be(233168)
+  }
+
+  it should "sum multiples of 7, 13 or 17 up to 20" in {
+    val multiplesSum = SumOfMultiples.sum(Set(7, 13, 17), 20)
+    multiplesSum should be(51)
+  }
+
+  it should "sum multiples of 4 or 6 up to 15" in {
+    val multiplesSum = SumOfMultiples.sum(Set(4, 6), 15)
+    multiplesSum should be(30)
+  }
+
+  it should "sum multiples of 5, 6 or 8 up to 150" in {
+    val multiplesSum = SumOfMultiples.sum(Set(5, 6, 8), 150)
+    multiplesSum should be(4419)
+  }
+
+  it should "sum multiples of 5 or 25 up to 51" in {
+    val multiplesSum = SumOfMultiples.sum(Set(5, 25), 51)
+    multiplesSum should be(275)
+  }
+
+  it should "sum multiples of 43 or 47 up to 10000" in {
+    val multiplesSum = SumOfMultiples.sum(Set(43, 47), 10000)
+    multiplesSum should be(2203160)
+  }
+
+  it should "sum multiples of 1 up to 100" in {
+    val multiplesSum = SumOfMultiples.sum(Set(1), 100)
+    multiplesSum should be(4950)
+  }
+
+  it should "sum multiples of an empty set up to 10000" in {
+    val multiplesSum = SumOfMultiples.sum(Set.empty, 10000)
+    multiplesSum should be(0)
+  }
 }
