94ebea42df889f4775547274bd7360e573352af9
Dennis Bueso-Calix <dlbcalix@gmail.com>
2018-02-12 12:13:10 +0000

    More testing of SumOfMultiples.

--- src/test/scala/worksheet01/SumOfMultiplesTest.scala
+++ src/test/scala/worksheet01/SumOfMultiplesTest.scala
@@ -8,4 +8,9 @@ class SumOfMultiplesTest extends FlatSpec with Matchers {
     val multiplesSum = SumOfMultiples.sum(Set(3, 5), 1)
     multiplesSum should be(0)
   }
+
+  it should "sum multiples of 3 or 5 up to 4" in {
+    val multiplesSum = SumOfMultiples.sum(Set(3, 5), 4)
+    multiplesSum should be(3)
+  }
 }
