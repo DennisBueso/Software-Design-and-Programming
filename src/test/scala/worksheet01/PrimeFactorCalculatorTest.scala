7ea473ed72498e9c91aa83f624facbd2f9cbcebc
Dennis Bueso-Calix <dlbcalix@gmail.com>
2018-02-13 01:20:13 +0000

    More testing on PrimeFactorCalculator.

--- src/test/scala/worksheet01/PrimeFactorCalculatorTest.scala
+++ src/test/scala/worksheet01/PrimeFactorCalculatorTest.scala
@@ -11,4 +11,24 @@ class PrimeFactorCalculatorTest extends FlatSpec with Matchers {
   it should "calculate that a prime number has exactly one prime factor" in {
     PrimeFactorCalculator.primeFactors(2) should be(List(2))
   }
+
+  it should "calculate that a square of a prime has exactly one prime factor repeated twice" in {
+    PrimeFactorCalculator.primeFactors(9) should be(List(3, 3))
+  }
+
+  it should "calculate that a cube of a prime has exactly one prime factor repeated thrice" in {
+    PrimeFactorCalculator.primeFactors(8) should be(List(2, 2, 2))
+  }
+
+  it should "ensure that a product of primes and non-primes is factored properly" in {
+    PrimeFactorCalculator.primeFactors(12) should be(List(2, 2, 3))
+  }
+
+  it should "ensure that a product of small primes is factored properly" in {
+    PrimeFactorCalculator.primeFactors(901255) should be(List(5, 17, 23, 461))
+  }
+
+  it should "ensure that a product of small and large primes is factored properly" in {
+    PrimeFactorCalculator.primeFactors(93819012551L) should be(List(11, 9539, 894119))
+  }
 }
