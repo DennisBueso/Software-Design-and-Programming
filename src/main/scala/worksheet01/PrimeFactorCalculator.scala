244b8b737381fad42830e6cd248b2f29a0521798
Dennis Bueso-Calix <dlbcalix@gmail.com>
2018-02-13 01:03:49 +0000

    More testing on PrimeFactorCalculator.

--- src/main/scala/worksheet01/PrimeFactorCalculator.scala
+++ src/main/scala/worksheet01/PrimeFactorCalculator.scala
@@ -2,5 +2,19 @@ package worksheet01
 
 object PrimeFactorCalculator {
 
-  def primeFactors(input: Int): List[Int] = List.empty
+  def primeFactors(input: Int): List[Int] = {
+
+    def calculate(remainder: Int, divisor: Int = 2): List[Int] = {
+      if (remainder < 2) {
+        List.empty
+      } else {
+        remainder % divisor match {
+          case 0 => divisor :: calculate(remainder / divisor, divisor)
+          case _ => calculate(remainder, divisor + 1)
+        }
+      }
+    }
+
+    calculate(input)
+  }
 }
--- src/test/scala/worksheet01/PrimeFactorCalculatorTest.scala
+++ src/test/scala/worksheet01/PrimeFactorCalculatorTest.scala
@@ -7,4 +7,8 @@ class PrimeFactorCalculatorTest extends FlatSpec with Matchers {
   "A Prime Factor Calculator" should "calculate that 1 has no prime factors" in {
     PrimeFactorCalculator.primeFactors(1) should be(List.empty)
   }
+
+  it should "calculate that a prime number has exactly one prime factor" in {
+    PrimeFactorCalculator.primeFactors(2) should be(List(2))
+  }
 }
