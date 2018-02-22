c435edd750af22e784266e53186d741545617f5e
Dennis Bueso-Calix <dlbcalix@gmail.com>
2018-02-22 01:55:16 +0000

    Edited Abstract Factory Design Pattern.

--- src/main/scala/designpatterns/creational/abstractfactory/AbstractParserFactory.scala
+++ src/main/scala/designpatterns/creational/abstractfactory/AbstractParserFactory.scala
@@ -6,26 +6,26 @@ trait AbstractParserFactory {
 
 class NYCParserFactory extends AbstractParserFactory {
   override def getParserInstance(parserType: String): XMLParser = parserType match {
-    case "NYCERROR" => print("NYC Parsing Error XML\nNYC ")
+    case "NYCERROR" => print("NYC Parsing Error XML...\nNYC ")
       new ErrorXMLParser
-    case "NYCFEEDBACK" => print("NYC Parsing Feedback XML\nNYC ")
+    case "NYCFEEDBACK" => print("NYC Parsing Feedback XML...\nNYC ")
       new FeedbackXMLParser
-    case "NYCORDER" => print("NYC Parsing Order XML\nNYC ")
+    case "NYCORDER" => print("NYC Parsing Order XML...\nNYC ")
       new OrderXMLParser
-    case "NYCRESPONSE" => print("NYC Parsing Response XML\nNYC ")
+    case "NYCRESPONSE" => print("NYC Parsing Response XML...\nNYC ")
       new ResponseXMLParser
   }
 }
 
 class LondonParserFactory extends AbstractParserFactory {
   override def getParserInstance(parserType: String): XMLParser = parserType match {
-    case "LondonERROR" => print("London Parsing Error XML\nLondon ")
+    case "LondonERROR" => print("London Parsing Error XML...\nLondon ")
       new ErrorXMLParser
-    case "LondonFEEDBACK" => print("London Parsing Feedback XML\nLondon ")
+    case "LondonFEEDBACK" => print("London Parsing Feedback XML...\nLondon ")
       new FeedbackXMLParser
-    case "LondonORDER" => print("London Parsing Order XML\nLondon ")
+    case "LondonORDER" => print("London Parsing Order XML...\nLondon ")
       new OrderXMLParser
-    case "LondonRESPONSE" => print("London Parsing Response XML\nLondon ")
+    case "LondonRESPONSE" => print("London Parsing Response XML...\nLondon ")
       new ResponseXMLParser
   }
 }
