5fc33055348a29025f40dfb5a17111b3045d959a
Dennis Bueso-Calix <dlbcalix@gmail.com>
2018-02-27 15:43:34 +0000

    Finished Builder Pattern.

---
+++ src/main/scala/designpatterns/creational/builder/CarDirector.scala
@@ -0,0 +1,16 @@
+package designpatterns.creational.builder
+
+// TODO
+
+case class CarDirector(carBuilder: CarBuilder) {
+  def build(): Unit = {
+    carBuilder.buildBodyStyle()
+    carBuilder.buildPower()
+    carBuilder.buildEngine()
+    carBuilder.buildBreaks()
+    carBuilder.buildSeats()
+    carBuilder.buildWindows()
+    carBuilder.buildFuelType()
+  }
+
+}
---
+++ src/main/scala/designpatterns/creational/builder/SportsCarBuilder.scala
@@ -0,0 +1,28 @@
+package designpatterns.creational.builder
+
+// TODO
+
+object SportsCarBuilder extends CarBuilder {
+  val sports = Car("SPORTS")
+
+  override def buildBodyStyle(): Unit = sports.setBodyStyle("External dimensions: overall length (inches): 192.3,\n" +
+    "   overall width (inches): 75.5, overall height (inches): 54.2,\n" +
+    "   wheelbase (inches): 112.3, front track (inches): 63.7,\n" +
+    "   rear track (inches): 64.1 and curb to curb turning circle (feet): 37.7")
+
+  override def buildPower(): Unit = sports.setPower("323 hp @ 6,800 rpm; 278 ft lb of torque @ 4,800 rpm")
+
+  override def buildEngine(): Unit = sports.setEngine("3.6L V 6 DOHC and variable valve timing")
+
+  override def buildBreaks(): Unit = sports.setBreaks("Four-wheel disc brakes: two ventilated. Electronic brake distribution.\n" +
+    "   Stability control")
+
+  override def buildSeats(): Unit = sports.setSeats("Driver sports front seat with one power adjustments manual height,\n" +
+    "   front passenger seat sports front seat with one power adjustments")
+
+  override def buildWindows(): Unit = sports.setWindows("Front windows with one-touch on two windows")
+
+  override def buildFuelType(): Unit = sports.setFuelType("Petrol 17 MPG city, 28 MPG highway, 20 MPG combined and 380 mi. range")
+
+  override def getCar: Car = sports
+}
